d9b580e616b5787399c5d9174c9c72bb57a5dff4
DerekGoodman721 <derekgoodman@outlook.com>
2025-08-29 21:16:04 -0400

    Add files via upload

---
+++ Course 1c/classy-shapes-project/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    double width;
+    double length;
+
+    public Rectangle(){
+        this.length = 1.0;
+        this.width = 1.0;
+    }
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+}
