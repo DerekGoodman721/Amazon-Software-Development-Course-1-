2966cd35827528147781f69478a6e1cf66cb0e22
DerekGoodman721 <derekgoodman@outlook.com>
2025-08-29 21:21:33 -0400

    Add files via upload

---
+++ Course 1c/fiteness-app-project/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        BMICalculator bmiCalculator = new BMICalculator(); 
+
+        double weightInPounds = 154.0;
+        double heightInInches =  70.0;
+        double bmiImperial = bmiCalculator.calculateBmiImperial(weightInPounds, heightInInches);
+
+        double weightInKilos = 70.0;
+        double heightInMeters = 1.84;
+        double bmiMetric = bmiCalculator.calculateBmiMetric(weightInKilos, heightInMeters);
+
+        System.out.println("BMI in Imperial System " + bmiImperial);
+        System.out.println("BMI in Metric: " + bmiMetric);
+    }
+}
